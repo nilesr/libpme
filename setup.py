f8d3d5d876ada799f7cf449fec1665f32bf55bff
Niles Rogoff <nilesrogoff@gmail.com>
2016-12-22 12:58:03 -0800

    Added setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,10 @@
+#from distutils.core import setup
+from setuptools import setup
+setup(name='libpme',
+      version='1.0',
+	  description="PNG metadata editor library",
+	  author="Niles Rogoff",
+	  author_email="me@niles.xyz",
+	  url="http://github.com/nilesr/libpme",
+      py_modules=['libpme'],
+      )
